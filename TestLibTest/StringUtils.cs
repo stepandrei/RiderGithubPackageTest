aed712603d6002c6dac4df2791555364af623223
Andrei Stepanov <andrei.stepanov@jetbrains.com>
2025-03-27 14:19:47 +0100

    package source code

---
+++ TestLibTest/StringUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TestLibTest
+{
+    public class StringUtils
+    {
+        public static string ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public static string ReverseString2(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+        
+        public static string ReverseString3(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
